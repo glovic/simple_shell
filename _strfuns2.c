4415d3181c7752196bd3f60743b73d4f9f73970e
Victor Nwaogu <xtineviktor@gmail.com>
2023-12-14 08:16:41 -0800

    Updateing string manipulation functions

--- _strfuns2.c
+++ _strfuns2.c
@@ -1,105 +1,161 @@
 #include "shell.h"
 
-char *_strchr(const char *s, char c);
-int _strspn(char *s, char *accept);
-int _strcmp(char *s1, char *s2);
-int _strncmp(const char *s1, const char *s2, size_t n);
-
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
+ * _strncmp - compare at most n bytes of two strings
+ * @s1: the first string to be compared
+ * @s2: the second string to be compared
+ * @n: the maximum number of bytes to compare
  *
- * Return: If c is found - a pointer to the first occurence.
- *         If c is not found - NULL.
- */
-char *_strchr(const char *s, char c)
-{
-	int index;
-
-	for (index = 0; s[index]; index++)
-	{
-		if (s[index] == c)
-			return (char *) (s + index);
-	}
-
-	return (NULL);
-}
-
-/**
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be searched.
- * @accept: The prefix to be measured.
- *
- * Return: The number of bytes in s which
- *         consist only of bytes from accept.
- */
-int _strspn(char *s, char *accept)
-{
-	int bytes = 0;
-	int index;
-
-	while (*s)
-	{
-		for (index = 0; accept[index]; index++)
-		{
-			if (*s == accept[index])
-			{
-				bytes++;
-				break;
-			}
-		}
-		s++;
-	}
-	return (bytes);
-}
-
-/**
- * _strcmp - Compares two strings.
- * @s1: The first string to be compared.
- * @s2: The second string to be compared.
- *
- * Return: Positive byte difference if s1 > s2
- *         0 if s1 = s2
- *         Negative byte difference if s1 < s2
- */
-int _strcmp(char *s1, char *s2)
-{
-	while (*s1 && *s2 && *s1 == *s2)
-	{
-		s1++;
-		s2++;
-	}
-
-	if (*s1 != *s2)
-		return (*s1 - *s2);
-
-	return (0);
-}
-
-/**
- * _strncmp - Compare two strings.
- * @s1: Pointer to a string.
- * @s2: Pointer to a string.
- * @n: The first n bytes of the strings to compare.
- *
- * Return: Less than 0 if s1 is shorter than s2.
- *         0 if s1 and s2 match.
- *         Greater than 0 if s1 is longer than s2.
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         or a positive value if s1 is greater than s2
  */
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
+	size_t i = 0;
+
+	while (*s1 == *s2 && i < n)
+	{
+		if (*s1 == '\0' || i + 1 == n)
+		{
+			/* both strings are equal or reached the end of comparison */
+			return (0);
+		}
+		s1++;
+		s2++;
+		i++;
+	}
+
+	return ((*s1) - (*s2));
+}
+
+/**
+ * _strchr - locate character in string
+ * @s: the string
+ * @c: the character to search
+ *
+ * Description: The _strchr() function returns a pointer to the first
+ * occurrence of the character @c in the string @s.
+ *
+ * Return: a pointer to the matched character or NULL if the character
+ * is not found
+ */
+char *_strchr(const char *s, int c)
+{
+	char *tmp_s = (char *)s;
+
+	/* handle empty string */
+	if (s == NULL)
+		return (NULL);
+
+	while (*tmp_s != '\0')
+	{
+		if (*tmp_s == c)
+			return (tmp_s); /* match found */
+		tmp_s++;
+	}
+
+	/* return a pointer to the null byte if 'c' is a null byte. */
+	if (c == '\0')
+		return (tmp_s);
+
+	return (NULL); /* no match found or end of string - return NULL */
+}
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: substring
+ *
+ * Description: The strstr() function finds the first occurrence of the
+ * substring @needle in the string @haystack. The terminating null bytes('\\0')
+ * are not compared.
+ *
+ * Return: return a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
+ */
+char *_strstr(const char *haystack, const char *needle)
+{
+	size_t i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		j = 0;
+
+		/* keep searching so long as characters in both strings are equal */
+		while (haystack[i + j] == needle[j] && needle[j])
+			j++;
+
+		/* end of substring reached - it means there was a full match */
+		if (needle[j] == '\0')
+		{
+			return ((char *)&haystack[i]);
+		}
+	}
+
+	return (NULL); /* no match found */
+}
+
+/**
+ * _strdup - Duplicates a string.
+ * @str: string to duplicate
+ *
+ * Description: The _strdup() function returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string @str. Memory for the
+ * new string is obtained with malloc() and can be freed with free()
+ *
+ * Return: A pointer to the duplicated string, NULL if it fails
+ */
+char *_strdup(const char *str)
+{
+	char *dup_str;
+	size_t i = 0;
+
+	if (str == NULL)
+		return (NULL); /* handle invalid string */
+
+	dup_str = malloc(_strlen(str) + 1);
+
+	if (dup_str == NULL)
+		return (NULL);
+
+	/* duplicate string - a copy process */
+	while (str[i])
+	{
+		dup_str[i] = str[i];
+		i++;
+	}
+	dup_str[i] = '\0';
+
+	return (dup_str);
+}
+
+/**
+ * _strncpy - copy a string
+ * @dest: destination string buffer
+ * @src: source string buffer
+ * @n: number of bytes to write
+ *
+ * Description: Copy @n bytes from the string @s to @dest. If there is no null
+ * byte among the first @n bytes of @s, the string in @dest will not be
+ * null-terminated.
+ *
+ * Return: a pointer to the destination string @dest
+ */
+char *_strncpy(char *dest, const char *src, size_t n)
+{
 	size_t i;
 
-	for (i = 0; s1[i] && s2[i] && i < n; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (s1[i] > s2[i])
-			return (s1[i] - s2[i]);
-		else if (s1[i] < s2[i])
-			return (s1[i] - s2[i]);
+		dest[i] = src[i];
 	}
-	if (i == n)
-		return (0);
-	else
-		return (-15);
+
+	while (i < n)
+	{
+		/* ensure exactly n bytes are written */
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
